630f12582823b1b5a9a1cb2a553c5ed5243b8b49
zzzel <marko@zzzel.xyz>
2016-03-30 18:53:24 +0200

    bugfix: stageView alpha reset to 1.0 after delete

--- !procrastinate/TaskCell.swift
+++ !procrastinate/TaskCell.swift
@@ -45,6 +45,7 @@ class TaskCell: UITableViewCell {
 	func handlePan(recognizer: UIPanGestureRecognizer) {
 		if recognizer.state == .Began {
 			originalCenter = center
+			stageView.alpha = 1.0
 		}
 		if recognizer.state == .Changed {
 			let translation = recognizer.translationInView(self)
@@ -79,7 +80,7 @@ class TaskCell: UITableViewCell {
 					UIView.animateWithDuration(0.2, animations: {
 						self.stageView.alpha = 0.0
 						}, completion: { _ in
-//							self.stageView.removeFromSuperview()
+							self.stageView.removeFromSuperview()
 					})
 				}
 			} else {
